7fabd603bb24d882133888d70773ba0f3d225630
Rafal Pawlowski <rafal.pawlowski1993@gmail.com>
2020-08-04 12:35:52 +0100

    Models and storage initialized

---
+++ LibraryMVC/LibraryMVC.Core/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMVC.Core.Models
+{
+    public class Book : BaseEntity
+    {
+        [StringLength(30)]
+        [DisplayName("Book Title")]
+        public String title { get; set; }
+        public String writerFirstName { get; set; }
+        public String writerLastName { get; set; }
+        public DateTime releaseDate { get; set; }
+        public String publisher { get; set; }
+        public int numberOfBooks { get; set; }
+        public String genre { get; set; }
+        public List<Format> formats { get; set; }
+        public String description { get; set; }
+        public static int count { get; set; }
+
+        public Book()
+        {
+
+        }
+    }
+}
---
+++ LibraryMVC/LibraryMVC.Core/Models/Format.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMVC.Core.Models
+{
+    public class Format : BaseEntity
+    {
+        public String name { get; set; }
+
+        public Format()
+        {
+
+        }
+    }
+}
