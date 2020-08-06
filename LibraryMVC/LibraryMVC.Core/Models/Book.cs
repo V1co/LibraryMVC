24c415f80cd3908ff87695bd764a77f164022136
Rafal Pawlowski <rafal.pawlowski1993@gmail.com>
2020-08-06 17:02:22 +0100

    Fixed management pages and fixed views

--- LibraryMVC/LibraryMVC.Core/Models/Book.cs
+++ LibraryMVC/LibraryMVC.Core/Models/Book.cs
@@ -11,22 +11,37 @@ namespace LibraryMVC.Core.Models
     public class Book : BaseEntity
     {
         [StringLength(30)]
+        [Required]
         [DisplayName("Book Title")]
         public String Title { get; set; }
+        [DisplayName("Writer First Name")]
+        [Required]
         public String WriterFirstName { get; set; }
+        [DisplayName("Writer Last Name")]
+        [Required]
         public String WriterLastName { get; set; }
+        [DisplayName("Release Date")]
+        [Required]
         public DateTime ReleaseDate { get; set; }
+        [DisplayName("Publisher")]
+        [Required]
         public String Publisher { get; set; }
+        [DisplayName("Number of Books")]
+        [Required]
         public int NumberOfBooks { get; set; }
+        [Required]
         public String Genre { get; set; }
+        //[Required] trzeba stworzyc formaty
         public List<Format> Formats { get; set; }
+        [Required]
         public String Description { get; set; }
+        
         public String Image { get; set; }
         public static int Count { get; set; }
 
         public Book()
         {
-
+            this.Id = Guid.NewGuid().ToString();
         }
     }
 }
