480524a8161584f1a6228dbaaf74df5cfcef2eec
Rafal Pawlowski <rafal.pawlowski1993@gmail.com>
2020-08-10 20:23:13 +0100

    Index page updated, new books created

--- LibraryMVC/LibraryMVC.WebUI/Controllers/HomeController.cs
+++ LibraryMVC/LibraryMVC.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LibraryMVC.Core.Contracts;
+using LibraryMVC.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,22 @@ namespace LibraryMVC.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+
+        IRepository<Book> context;
+        IRepository<BookFormat> bookFormats;
+        IRepository<BookGenre> bookGenres;
+
+        public HomeController(IRepository<Book> Context, IRepository<BookFormat> BookFormats, IRepository<BookGenre> BookGenres)
+        {
+            context = Context;
+            bookFormats = BookFormats;
+            bookGenres = BookGenres;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<Book> books = context.Collection().ToList();
+            return View(books);
         }
 
         public ActionResult About()
